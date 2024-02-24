da7f67a850ff761ec2a8d2008e4f63dda0c9c3e7
Deepak Mahat <meepak@gmail.com>
2024-02-25 04:42:47 +1030

    some clean up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SvgDataProvider } from '@x/svg-data';
-import { Background } from '@c/background';
+import { Background, BackgroundOptions } from '@c/background';
 
 export default function RootLayout() {
   return (
     <SvgDataProvider>
-      <Background>
+      <Background option={BackgroundOptions.default}>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <Slot />
         </GestureHandlerRootView>
--- component/background/animated-color.tsx
+++ component/background/animated-color.tsx
@@ -6,13 +6,13 @@ import { Animated, StyleSheet } from 'react-native';
 export const AnimatedColorOptions = {
   splash: {
     duration: 500,
-    inputRange: [0, 1, 2],
-    outputRange: ['#4e164d', '#6f164d', '#7c517c'] //'#21164d', '#39164d', 
+    inputRange: [0, 1, 2, 3, 4, 5],
+    outputRange: ['#8f0fff', '#b57edc', '#cccfff', '#fae6fa', '#fff0f5', '#e6e6fa']
   },
   default: {
-    duration: 4000,
-    inputRange: [0, 1, 2, 3, 4, 5 ,6],
-    outputRange: ['#D1C4E9', '#E1BEE7', '#F8BBD0', '#FFCDD2', '#C8E6C9', '#B3E5FC', '#BBDEFB']
+    duration: 7777,
+    inputRange: [0, 1, 2, 3, 4, 5 ],
+    outputRange: [ '#f0f8ff', '#f2f3f4', '#fff8e7', '#f8f8ff', '#f5fffa', '#f7f7f7']
   }
 }
 
@@ -47,7 +47,9 @@ const AnimatedColor = ({option=AnimatedColorOptions.default, children}) => {
 
 
   return (
-    <Animated.View style={{...StyleSheet.absoluteFillObject, backgroundColor: interpolateColor}}>{children}</Animated.View>
+    <Animated.View style={{...StyleSheet.absoluteFillObject, backgroundColor: interpolateColor}}>
+      {children}
+      </Animated.View>
   );
 };
 
